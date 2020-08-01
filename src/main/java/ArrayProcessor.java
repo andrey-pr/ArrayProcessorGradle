f68280a20457501e33516d3153a05581958650d8
Andrey <andreykvak.g3@gmail.com>
2020-08-01 09:55:01 +0300

    ArrayProcessor

---
+++ src/main/java/ArrayProcessor.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArrayProcessor {
+    public static int[] process(int[] arr) {
+        ArrayList<Integer> al = new ArrayList<>();
+        for (int current : arr)
+            if (current % 2 == 0)
+                al.add(current);
+        Collections.sort(al);
+        Collections.reverse(al);
+        int[] newArray = new int[al.size()];
+        for (int i = 0; i < al.size(); i++)
+            newArray[i] = al.get(i);
+        return newArray;
+    }
+}
---
+++ src/test/java/ArrayProcessorTest.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArrayProcessorTest {
+
+    @Test
+    public void process1() {
+        assertArrayEquals(new int[]{}, ArrayProcessor.process(new int[]{}));
+    }
+
+    @Test
+    public void process2() {
+        assertArrayEquals(new int[]{4, 2}, ArrayProcessor.process(new int[]{5, 4, 3, 2, 1}));
+    }
+
+    @Test
+    public void process3() {
+        assertArrayEquals(new int[]{6, 4, 2}, ArrayProcessor.process(new int[]{2, 4, 6}));
+    }
+
+    @Test
+    public void process4() {
+        assertArrayEquals(new int[]{4, 2}, ArrayProcessor.process(new int[]{1, 2, 3, 4, 5}));
+    }
+
+    @Test
+    public void process5() {
+        assertArrayEquals(new int[]{10, 8, 6, 4, 2}, ArrayProcessor.process(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
+    }
+}

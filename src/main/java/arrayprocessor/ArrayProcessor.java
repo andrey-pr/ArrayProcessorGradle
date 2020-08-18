0d3b84742603457dae1f7099561419447a0a350c
Andrey <andreykvak.g3@gmail.com>
2020-08-18 11:40:18 +0300

    moved classes into packages

--- src/main/java/arrayprocessor/ArrayProcessor.java
+++ src/main/java/arrayprocessor/ArrayProcessor.java
@@ -1,3 +1,5 @@
+package arrayprocessor;
+
 import java.util.ArrayList;
 import java.util.Collections;
 
--- src/test/java/tests/arrayprocessor/ArrayProcessorTest.java
+++ src/test/java/tests/arrayprocessor/ArrayProcessorTest.java
@@ -1,3 +1,6 @@
+package tests.arrayprocessor;
+
+import arrayprocessor.ArrayProcessor;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
